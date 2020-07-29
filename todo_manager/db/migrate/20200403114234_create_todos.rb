8aff7edc296871f178d0e7f2a8a3f07009746de8
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-29 20:26:23 +0530

    Delete todo_app

--- todo_manager/db/migrate/20200403114234_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[6.0]
-  def change
-    create_table :todos do |t|
-      t.text :todo_text
-      t.date :due_date
-      t.boolean :completed
-    end
-  end
-end
