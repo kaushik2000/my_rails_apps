c6dd0f150dda88a6f350a3bb776abc4e6e10b53f
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-31 14:15:00 +0530

    Changes: Remove cookie creation for sign-up

--- gas_det_app/app/controllers/users_controller.rb
+++ gas_det_app/app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
       password: params[:password],
     )
     user.save
-    session[:current_user_id] = user.id
     redirect_to new_session_path 
   end
 end
