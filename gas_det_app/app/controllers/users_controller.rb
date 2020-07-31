e4a9d64a13cd200033aab8692de362d1b4ed45b7
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-31 13:13:32 +0530

    Create scaffold-User

---
+++ gas_det_app/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
+  def new
+    render "new"
+  end
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+    )
+    user.save
+    session[:current_user_id] = user.id
+    redirect_to "/"
+  end
+end
