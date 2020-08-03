8105a372c1c2297a8e2de631c0e5f3ea37643122
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-08-03 12:56:53 +0530

    Change table format : Users table

--- gas_det_app/db/migrate/20200731030738_create_users.rb
+++ gas_det_app/db/migrate/20200731030738_create_users.rb
@@ -1,13 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string "first_name"
-      t.string "last_name"
-      t.string "email"
-      t.string "password_digest"
-      t.string "role"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :role
+      t.timestamps
     end
   end
 end
